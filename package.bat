99fd6da9b8b1e222a9da82458b5b4eddd69f9a5d
zhenchao <mrzhencc@sina.com>
2020-02-15 23:11:24 +0800

    fix package

--- package.bat
+++ package.bat
@@ -1,11 +1,16 @@
 @echo off
 
 set dir=bin
+set install_dir=amp
+
+if not exist %install_dir% (
+    md %install_dir%
+)
 
 if not exist %dir% (
-	md %dir%
+    md %dir%
 ) else (
-	rd /s /q %dir% && md %dir%
+    rd /s /q %dir% && md %dir%
 )
 
 cd %dir%
@@ -14,3 +19,4 @@ copy ..\fav.ico .\
 copy ..\*.py .\
 
 pyinstaller -Fw login_window.py -i .\fav.ico -p E:\Python\Python38-32\Lib\site-packages\pywin32_system32
+copy .\dist\login_window.exe ..\%install_dir%
